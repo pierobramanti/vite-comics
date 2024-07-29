f9df1604ac78e5c9123f8bcb1667a191df0153b3
pierobramanti <piero.bramanti94@gmail.com>
2024-07-29 17:09:32 +0200

    [UPD] moved file.vue in components and create structure

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
-<script setup>
+<script>
+import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
+import Footer from "./components/Footer.vue"
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
 
 </script>
 
