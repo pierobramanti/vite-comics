6bfa1fc7864ed88ec98441981db7eaea9ae02617
pierobramanti <piero.bramanti94@gmail.com>
2024-07-29 18:23:07 +0200

    [UPD]  list added with bg image in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,8 +4,63 @@ export default {
 }
 </script>
 <template>
-
+    <footer class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <div class="d-flex">
+                        <div class="me-5">
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">DC Comics</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none text-secondary" href="#">Characters</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Comics</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Movies</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">TV</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Games</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Videos</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">News</a></li>
+                            </ul>
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">Shop</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none text-secondary" href="#">Shop DC</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div class="me-5">
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">DC</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none text-secondary" href="#">Terms Of Use</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Privacy policy (New)</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Ad Choises</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Advertising</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Jobs</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Subscription</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Talent Workshops</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">CPSC Certificates</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Ratings</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Ahop Help</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div class="me-5">
+                            <h4 class="py-3 text-white fw-bolder text-uppercase">Sites</h4>
+                            <ul class="list-unstyled">
+                                <li><a class="text-decoration-none text-secondary" href="#">DC</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">MAD Magazione</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">DC Kids</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">DC Universe</a></li>
+                                <li><a class="text-decoration-none text-secondary" href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 <style lang="scss">
-    
+footer {
+    background-image: url("../assets/footer-bg.jpg");
+    background-size: cover;
+}
 </style>
