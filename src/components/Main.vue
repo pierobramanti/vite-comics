ced7df3db017a06113713e34eec982add3d677f1
pierobramanti <piero.bramanti94@gmail.com>
2024-08-04 15:59:49 +0200

    [UPD] hero component created

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,81 +1,45 @@
 <script>
-    export default {
-    data() {
-        return {
-            shopItems: [
-                {
-                    href: "#",
-                    src: "./src/assets/buy-comics-digital-comics.png",
-                    text: "Digital Comics"
-                },
-                {
-                    href: "#",
-                    src: "./src/assets/buy-comics-merchandise.png",
-                    text: "DC Merchandise"
-                },
-                {
-                    href: "#",
-                    src: "./src/assets/buy-comics-subscriptions.png",
-                    text: "Subscription"
-                },
-                {
-                    href: "#",
-                    src: "./src/assets/buy-comics-shop-locator.png",
-                    text: "Comic Shop Locator"
-                },
-                {
-                    href: "#",
-                    src: "./src/assets/buy-dc-power-visa.svg",
-                    text: "DC Power Visa"
-                },
-            ]
-        }
+import BlueSection from '../components/partials/Blue-section.vue';
+import Hero from '../components/partials/Hero.vue';
+export default {
+    components: {
+        Hero,
+        BlueSection
     }
 }
 </script>
 <template>
     <main>
-        <div class="bg-dark py-5 ">
+        <Hero />
+        <div class="bg-color">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <h2 class="text-light"> --> Content goes here <--</h2>
+                            <h2 class="text-light py-5">--> Content goes here <--</h2>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="bg-blue position-relative z-3 ">
+        <div class="bg_blue">
             <div class="container">
-                <div class="row">
+                 <div class="row">
                     <div class="col-12">
-                        <div class="py-5">
-                            <div class="d-flex align-items-center justify-content-around">
-                                <div v-for="item, index in shopItems" :key="`shop-${index}`" class="d-flex align-items-center ms-4">
-                                    <a class="w_square me-3" :href="item.href">
-                                        <img :src="item.src" :alt="item.text">
-                                    </a>
-                                    <p class="m-0 text-uppercase text-white">{{ item.text }}</p>
-                                </div>
-                            </div>
-                        </div>
+                        <BlueSection />
                     </div>
                 </div>
             </div>
         </div>
+        
     </main>
 </template>
 <style lang="scss" scoped>
- .d-flex {
-    a {
-        img {
-            max-width: 100%;
-            max-height: 100%;
-            object-fit: contain;
-            object-position: center
+    @use "../components/styles/partials/variables.scss";
+    main {
+        .bg-color {
+            background-color: variables.$main_bg;
         }
-    }
-}
-    .bg-blue{
-        background-color: blue;
+        .bg_blue {
+            background-color: variables.$primary_color;
+        }
     }
 </style>
---
+++ src/components/partials/Hero.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="bg-hero"></div>
+</template>
+<style lang="scss">
+    .bg-hero {
+        background-image: url("../../assets/jumbotron.jpg");
+        background-size: cover;
+        height: 400px;
+    }
+</style>
