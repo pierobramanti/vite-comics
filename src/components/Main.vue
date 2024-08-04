11d81135fbe2732980186a17444f3338208464ea
pierobramanti <piero.bramanti94@gmail.com>
2024-08-04 17:40:29 +0200

    [UPD] working on content section and button added

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,25 +1,32 @@
 <script>
-import BlueSection from '../components/partials/Blue-section.vue';
+import BlueSection from '../components/partials/Blue-section.vue'
 import Hero from '../components/partials/Hero.vue';
 import ComicsSection from '../components/partials/ComicSection.vue';
-
 import comics from '../components/data/comicList';
 export default {
     components: {
         Hero,
         ComicsSection,
         BlueSection
+    },
+    data() {
+        return {
+            comics
+        }
     }
 }
 </script>
 <template>
     <main>
         <Hero />
-        <div class="bg-color">
+        <div class="bg-color pt-5">
             <div class="container">
                 <div class="row">
+                    <ComicsSection v-for="thumb, index in comics" :key="`t-${index}`" :cover="thumb"/>
                     <div class="col-12">
-                        <ComicsSection v-for="thumb, index in comics" :key="`t-${index}`" :cover="thumb"/>
+                        <div class="d-flex justify-content-center my-4">
+                            <button class="btn text-uppercase fw-bolder text-white rounded-0">Load More</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -37,10 +44,23 @@ export default {
     </main>
 </template>
 <style lang="scss" scoped>
-    @use "../components/styles/partials/variables.scss";
+    @use "../components//styles/partials/variables.scss";
     main {
         .bg-color {
             background-color: variables.$main_bg;
+            .col-12 {
+                button {
+                    background-color: variables.$primary_color;
+                    border: 1px solid variables.$primary_color;
+                    padding: 10px 60px;
+                    transition: all 0.3 ease;
+                    &:hover {
+                        background-color: #ffffffa1;
+                        color: #fff;
+                        border: 1px solid #ffffffa1
+                    }
+                }
+            }
         }
         .bg_blue {
             background-color: variables.$primary_color;
